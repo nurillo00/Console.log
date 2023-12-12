76a9ac635485445983a56f2596673dbf13464d83
nurillo00 <nurillobest33@gmail.com>
2023-12-12 09:45:06 +0500

    console.log

---
+++ js/index.js
@@ -0,0 +1,13 @@
+alert('IT oshxonasiga xush kelibsiz!')
+const elMenu = prompt('Ovqat tanlang')
+
+if (elMenu == 'somsa') {
+    console.log('5000 som');
+}  else if (elMenu == 'gumma') {
+    console.log('3000 som');
+} else if (elMenu == 'pirashka') {
+    console.log('1500 som');
+} else if (elMenu == 'cheburek') {
+    console.log('7000 som');
+}
+
